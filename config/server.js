5fa9898342c8d3acb58736ad944e2eda1cd9b639
Jonas Rotert <jonas@rotert.online>
2020-12-31 21:38:45 +0100

    Initial strapi installation

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1049bf8d3f565d181a6c26b155638d03'),
+    },
+  },
+});
